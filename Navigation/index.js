e2ed2aebcf8e1a519e838d8d6e1cb4244c44d123
leobrunorolon <leobrunorolon@gmail.com>
2022-06-09 22:26:28 -0300

    main push

--- Navigation/index.js
+++ Navigation/index.js
@@ -2,8 +2,8 @@ import React, { useState } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import TabNavigatorLogged from "./Tabs/UserLogged";
 import { SafeAreaView } from "react-native-safe-area-context";
-import AuthStack from "./Stacks/Auth";
 import { useSelector } from "react-redux";
+import AuthStack from "./Stacks/Auth";
 
 const MainNavigator = () => {
   const { user } = useSelector((state) => state.auth.value);
